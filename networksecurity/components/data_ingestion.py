fca8baaa2ef02fef86488790a67eda4e194777ca
Sai Teja <saitejapochana2324.com>
2025-06-25 22:59:12 +0530

    Data Validation

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -27,9 +27,7 @@ class DataIngestion:
             raise NetworkSecurityException(e,sys)
         
     def export_collection_as_dataframe(self):
-        """
-        Read data from mongodb
-        """
+        ''' Reading Data from MongoDB '''
         try:
             database_name=self.data_ingestion_config.database_name
             collection_name=self.data_ingestion_config.collection_name
@@ -93,7 +91,7 @@ class DataIngestion:
             dataframe=self.export_collection_as_dataframe()
             dataframe=self.export_data_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe)
-            dataingestionartifact=DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
+            dataingestionartifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
                                                         test_file_path=self.data_ingestion_config.testing_file_path)
             return dataingestionartifact
 
